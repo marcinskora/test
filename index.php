4394acd6abae211d34bc5fbf9a043ecc9bb5a3b8
Marcin <Marcin@skora>
2015-12-02 20:02:43 +0100

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -12,4 +12,4 @@ echo 'SUPER data: ' . date("Y-m-d H:i:s") . ' KONIEC DATY';
 echo 'TEST TEST nowa zmiana';
 echo 'nowa linijka';
 echo 'super nowa linijka';
-echo 'super test 4567890123';
+echo 'super test 4567890123456';
